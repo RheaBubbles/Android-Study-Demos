3dd587feaa01a5cf77885feb4371db10441bf1be
RheaBubbles <augbubbles@foxmail.com>
2018-07-07 01:24:23 +0800

    Add a sample RecyclerView in Activity-Demos for list all demos.

--- ActivityDemos/app/src/main/java/com/hit/bubbl/activitydemos/MainActivity.java
+++ ActivityDemos/app/src/main/java/com/hit/bubbl/activitydemos/MainActivity.java
@@ -2,12 +2,47 @@ package com.hit.bubbl.activitydemos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String[] mDataset;
+    private static final int DATASET_COUNT = 60;
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initDataset();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.activity_demos_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(mDataset);
+
+        // set the adapter to RecyclerView
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+    private void initDataset() {
+        mDataset = new String[DATASET_COUNT];
+        for (int i = 0; i < DATASET_COUNT; i++) {
+            mDataset[i] = "This is element #" + i;
+        }
     }
 }
